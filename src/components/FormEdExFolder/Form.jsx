8236dd2d7a388536832ef96b340ad609ca80c26e
Cecilia Christina <cecilia.lares@bootcamp-upgrade.com>
2024-07-05 21:52:14 +0200

    finished react

--- src/components/FormEdExFolder/Form.jsx
+++ src/components/FormEdExFolder/Form.jsx
@@ -4,24 +4,38 @@ import Experience from "./Experience";
 import "./styleForm.scss";
 
 function Form({ dataEducation, dataExperience }) {
-    const [cssVisibilityEd, setCssVisibilityEd] = useState("hidden");
-    const [cssVisibilityEx, setCssVisibilityEx] = useState("visibility");
+    const [cssVisibilityEd, setVisibilityEd] = useState("hidden");
+    const [cssVisibilityEx, setVisibilityEx] = useState("visibility");
+    const experience = "experience";
+    const education = "education";
 
-    const handleClick = () => {
-        cssVisibilityEd === "visibility" ? setCssVisibilityEd("hidden") : setCssVisibilityEd("visibility");
-        cssVisibilityEx === "hidden" ? setCssVisibilityEx("visibility") : setCssVisibilityEx("hidden");
+    const handleClick = (ev) => {
+        /* cssVisibilityEd === "visibility" ? setVisibilityEd("hidden") : setVisibilityEd("visibility");
+        cssVisibilityEx === "hidden" ? setVisibilityEx("visibility") : setVisibilityEx("hidden"); */
+        const type = ev.target.value;
+        if (type === "education") {
+            setVisibilityEd("visibility");
+            setVisibilityEx("hidden");
+        } else {
+            setVisibilityEd("hidden");
+            setVisibilityEx("visibility");
+        }
     };
 
     console.log(`Educación: ${cssVisibilityEd} Experiencia: ${cssVisibilityEx}`);
     return (
         <section>
-            <article className={cssVisibilityEd}>
-                <button onClick={handleClick} >Prueba education</button>
-                <Education dataEducation={dataEducation} />
+            <article >
+                <button onClick={handleClick} value={education}>Prueba education</button>
+                <div className={cssVisibilityEd}>
+                    <Education dataEducation={dataEducation} />
+                </div>
             </article>
-            <article className={cssVisibilityEx}>
-                <button onClick={handleClick} >Prueba experience</button>
-                <Experience dataExperience={dataExperience} />
+            <article >
+                <button onClick={handleClick} value={experience}>Prueba experience</button>
+                <div className={cssVisibilityEx}>
+                    <Experience dataExperience={dataExperience} />
+                </div>
             </article>
         </section>
     )
